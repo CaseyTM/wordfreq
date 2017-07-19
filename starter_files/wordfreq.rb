00f97a90fd6218b840252af73fbd2fd66cc4e89d
Casey McCaskill <>
2017-07-19 16:03:31 -0400

    init commit

---
+++ starter_files/wordfreq.rb
@@ -0,0 +1,46 @@
+require 'csv'
+
+class Wordfreq
+  STOP_WORDS = ['a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from',
+    'has', 'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
+    'were', 'will', 'with']
+
+  def initialize(filename)
+       seneca = File.read("seneca_falls.txt")
+       seneca.downcase.gsub /\W+/, ' '
+       @seneca_format = seneca.split(" ")
+       emancipation = File.read("emancipation_proclamation.txt")
+       emancipation.downcase.gsub /\W+/, ' '
+       @emancipation_format = emancipation.split(" ")
+
+  end
+
+  def frequency(word)
+    puts word
+  end
+
+  def frequencies
+  end
+
+  def top_words(number)
+  end
+
+  def print_report
+
+  end
+end
+
+if __FILE__ == $0
+  filename = ARGV[0]
+  if filename
+    full_filename = File.absolute_path(filename)
+    if File.exists?(full_filename)
+      wf = Wordfreq.new(full_filename)
+      wf.print_report
+    else
+      puts "#{filename} does not exist!"
+    end
+  else
+    puts "Please give a filename as an argument."
+  end
+end
